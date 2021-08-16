b827f89ad304518652580b5b92567285b2d8f632
Luiz Gustavo <luiz.matias@upe.br>
2021-08-16 17:25:18 -0300

    refactor(pages): toggle the tester card when clean the regex array

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -13,47 +13,53 @@ import {Fade} from '@material-ui/core'
 export default function HomePage() {
   const [{getRegex, words}, {add, clear}] = useRegex()
   const [wordValue, setWordValue] = React.useState('')
-  const [regexValidator, setRegexValidator] = React.useState('')
+  const [regexTesterValue, setRegexTesterValue] = React.useState('')
   const {isOpen, onToggle} = useDisclosure()
 
   const {value: regexValue, regex} = getRegex() ?? {}
-  const isValidatorOk = regex?.test(regexValidator)
+  const hasTheRegexTesterValueChecker = regex?.test(regexTesterValue)
 
-  const handleChangeWords = (event) => setWordValue(event.target.value)
+  const handleChangeWord = (event) => setWordValue(event.target.value)
   const handleChangeRegexValidator = (event) =>
-    setRegexValidator(event.target.value)
+    setRegexTesterValue(event.target.value)
 
   const addNewWord = () => {
     add(wordValue)
     setWordValue('')
   }
 
+  const _clear = () => {
+    onToggle()
+    clear()
+  }
+
   return (
     <Container py="5">
       <Flex flexDirection="column" flex={1} justifyContent="space-between">
-        <Text fontSize="3xl">Gerador de expressões regulares</Text>
+        <Text fontSize="3xl">🤖 Gerador de expressões regulares</Text>
         <Text as="samp" fontSize="2xl" my="4">
           {regexValue ?? 'Digite algo no input abaixo...'}
         </Text>
         <Flex flexDirection="column">
           {words.map((word, index) => (
             <Text key={`${index}`} my={2}>
-              {word}
+              <li>{word}</li>
             </Text>
           ))}
         </Flex>
         <Input
           value={wordValue}
-          onChange={handleChangeWords}
+          onChange={handleChangeWord}
           my="4"
           variant="flushed"
+          placeholder="Bora tacar umas palavras aqui?"
         />
         <Flex mt="4" flex={1}>
           <Button flex={1} onClick={addNewWord} colorScheme="green">
             Adicionar
           </Button>
           <Box w="2" />
-          <Button flex={1} onClick={() => clear()} colorScheme="red">
+          <Button flex={1} onClick={() => _clear()} colorScheme="red">
             Limpar
           </Button>
         </Flex>
@@ -63,14 +69,16 @@ export default function HomePage() {
         </Button>
         <Fade in={isOpen}>
           <Box p="40px" mt="4" rounded="md" shadow="md">
-            <Text fontSize="2xl">
-              Sua regex está {isValidatorOk ? '✅' : '❌'}
+            <Text
+              fontSize="2xl"
+              color={hasTheRegexTesterValueChecker ? 'green' : 'red'}>
+              {hasTheRegexTesterValueChecker ? '✅ Deu match' : '❌ Ops'}
             </Text>
             <Input
               fontSize="2xl"
-              color={isValidatorOk ? 'green' : 'red'}
-              isInvalid={!isValidatorOk}
-              value={regexValidator}
+              color={hasTheRegexTesterValueChecker ? 'green' : 'red'}
+              isInvalid={!hasTheRegexTesterValueChecker}
+              value={regexTesterValue}
               onChange={handleChangeRegexValidator}
               variant="flushed"
               my="4"
